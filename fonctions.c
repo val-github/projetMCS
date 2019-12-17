06a0ab10fd5401cd48d496a5ab7db70cc8529a41
val-github <55361570+val-github@users.noreply.github.com>
2019-12-17 11:22:56 +0100

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -12,3 +12,27 @@
 */
 
 #include "poker.h"
+
+void dialogueClt(int sd, struct sockaddr_in clt)
+{
+    char requete[MAX_BUFF];
+    do
+    {
+        read(sd, buffer, sizeof(buffer));
+        sscanf(buffer, "%s:%s", requete, buffer);
+        switch (atoi(requete))
+        {
+        case 0:
+            printf("Au revoir client 1 sur service 6000\n");
+            break;
+        case 100:
+            write(sd, OK, strlen(OK) + 1);
+            printf("OK : message recu de la part de client 1 %s\n ", buffer);
+            break;
+        default:
+            write(sd, NOK, strlen(NOK) + 1);
+            printf("NOK : message recu %s\n", buffer);
+            break;
+        }
+    } while (atoi(requete) != 0);
+}
