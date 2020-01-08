16c74aa1112516aad9f318383b1d301a6673f7e9
Gwen <gwendoline.quettier@gmail.com>
2020-01-08 08:58:48 +0100

    poker

---
+++ poker.c
@@ -0,0 +1,13 @@
+#include "poker.h"
+#include "fonctions.c"
+
+/* PROTOTYPES */
+
+/* MAIN */
+int main()
+{
+    printf("Bonjour dans cette partie de poker\n");
+    return 0;
+}
+
+/* FONCTIONS */
